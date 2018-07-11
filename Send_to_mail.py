7dc1c05e46ac91903c972d9f1cabd4b7f40b1eb6
didenkovv <slavatorrent@ukr.net>
2018-07-11 17:55:07 +0300

    fix equals date creat file

--- Send_to_mail.py
+++ Send_to_mail.py
@@ -2,22 +2,31 @@ import os, glob
 import datetime
 import  time
 
-date = datetime.datetime.now().date()
+###### find files #######
 
+
+#################### this is system date##############
+WINservice = "WINservice"
+Serv2016_1C = "Serv2016_1C"
+print(Serv2016_1C)
+
+
+dates = datetime.datetime.now().date()
+####### find new file on the date #################
 os.chdir("D:\TEMP")
 for file in glob.glob("*.txt"):
     result = file.rsplit('_' ,-1 )
     test = result[-1][0:10]
-    if date.__eq__(date):
+    if dates.__eq__(test):
         print("work is vell")
     else:
         print("work os not vell")
 #print(date)
 
-
-a  = os.path.getmtime("12.txt")
+#### find darte from parametr file #####
+a = os.path.getmtime("12.txt")
 print(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
-print(date)
+print(dates)
 
 
 #print(str(a))
