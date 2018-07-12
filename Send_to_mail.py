9f822f5f52cdcc189838b50841eb06ce6a5bc3ae
didenkovv <slavatorrent@ukr.net>
2018-07-12 17:20:58 +0300

    test functions

--- Send_to_mail.py
+++ Send_to_mail.py
@@ -1,32 +1,48 @@
 import os, glob
 import datetime
+from os import listdir
 import  time
+
+### service name ####
+WINservice = "WINservice"
+Serv2016_1C = "Serv2016_1C"
+#print(Serv2016_1C)
+
+### find dir ####
+os.chdir("D:\TEMP")
 
 ###### find files #######
 
 
 #################### this is system date##############
-WINservice = "WINservice"
-Serv2016_1C = "Serv2016_1C"
-print(Serv2016_1C)
+
 
 
 dates = datetime.datetime.now().date()
 ####### find new file on the date #################
-os.chdir("D:\TEMP")
+
 for file in glob.glob("*.txt"):
-    result = file.rsplit('_' ,-1 )
-    test = result[-1][0:10]
-    if dates.__eq__(test):
-        print("work is vell")
+ #   a = os.path
+    result = file.rsplit('D' ,-1 )
+    tream = result[-1][0:10]
+    a = os.path.getmtime(file)
+    if dates==(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')):
+ #       print("work is vell")
+        print (dates, "work is well", datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
     else:
-        print("work os not vell")
+        print("work іs not vell")
 #print(date)
 
 #### find darte from parametr file #####
-a = os.path.getmtime("12.txt")
-print(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
-print(dates)
+
+#print(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
+#print(dates)
 
 
 #print(str(a))
+
+#files = listdir(os.chdir("D:\TEMP"))
+#print(files)
+for i in glob.glob("*.txt"):
+    print(i)
+
