7c1b047ef16f12ce95b759f11c3a9d3f8a231f2a
didenkovv <slavatorrent@ukr.net>
2018-07-13 13:02:03 +0300

    equals date and time files and system date

--- Send_to_mail.py
+++ Send_to_mail.py
@@ -16,33 +16,24 @@ os.chdir("D:\TEMP")
 
 #################### this is system date##############
 
-
-
-dates = datetime.datetime.now().date()
+dates = datetime.datetime.now().date().__str__()
 ####### find new file on the date #################
 
-for file in glob.glob("*.txt"):
- #   a = os.path
-    result = file.rsplit('D' ,-1 )
-    tream = result[-1][0:10]
-    a = os.path.getmtime(file)
-    if dates==(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')):
- #       print("work is vell")
-        print (dates, "work is well", datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
-    else:
-        print("work іs not vell")
-#print(date)
+#for file in glob.glob("*.txt"):
+# #   a = os.path
+#    result = file.rsplit('D' ,-1 )
+#    tream = result[-1][0:10]
+#    a = os.path.getmtime(file)
+#    if dates==(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')):
+# #      print("work is vell")
+#        print (dates, "work is well", datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
+#    else:
+#        print("work іs not vell")
 
-#### find darte from parametr file #####
-
-#print(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
-#print(dates)
-
-
-#print(str(a))
 
 #files = listdir(os.chdir("D:\TEMP"))
 #print(files)
-for i in glob.glob("*.txt"):
-    print(i)
-
+for filename in os.listdir("D:/TEMP"):
+    a = os.path.getmtime(filename)
+    if filename.endswith(".txt") and datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')==dates:
+        print("==>>")
