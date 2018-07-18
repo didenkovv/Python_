8df9140846cefe916ffd362a8258cef4ad258e7a
didenkovv <slavatorrent@ukr.net>
2018-07-18 17:36:55 +0300

    eqauls date and file

--- Send_to_mail.py
+++ Send_to_mail.py
@@ -1,46 +1,34 @@
 import os, glob
 import datetime
-from os import listdir
-import  time
 
-### service name ####
-WINservice = "WINservice"
-Serv2016_1C = "Serv2016_1C"
-#print(Serv2016_1C)
+### service name, need set beckup name machines ####
+listServiceBeckup = ["WINservice", "Serv2016_1C"]
 
-### find dir ####
-os.chdir("D:\TEMP")
+### find work dir ####
+workDir = "D:\TEMP"
 
-###### find files #######
+### found character in file name #######
+charFind = 'D'
 
+###### find file extension for chack #######
+extensionFile = ".txt"
 
-#################### this is system date##############
+##### list elements for equals emty list ####
+listBack = []
+
+#################### this is system date ##############
 dates = datetime.datetime.now().date().__str__()
-####### find new file on the date #################
-
-#for file in glob.glob("*.txt"):
-# #   a = os.path
-#    result = file.rsplit('D' ,-1 )
-#    tream = result[-1][0:10]
-#    a = os.path.getmtime(file)
-#    if dates==(datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')):
-# #      print("work is vell")
-#        print (dates, "work is well", datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d'))
-#    else:
-#        print("work іs not vell")
-
-
-#files = listdir(os.chdir("D:\TEMP"))
-#print(files)
 
 def chackDatafile(pathDir, *args ):
     for filename in os.listdir(pathDir):
         a = os.path.getmtime(filename)
-        if filename.endswith(".txt") and datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')==dates and filename.partition('D')[0].__eq__(args[1]):
-            print("-->> work is well")
-        else:
-            print("work is not well")
-            continue
- #           print(args[0])
+        if filename.endswith(extensionFile) and datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')==dates and listServiceBeckup.__contains__(filename.rsplit(charFind)[0]):
+            tempelementForEquals = filename.rsplit(charFind)[0]
+            listBack.append(tempelementForEquals)
+    balanceList = set(listServiceBeckup) - set(listBack)
+    if not balanceList:
+        print("have not element, -send to mail for secsesfol")
+    else:
+        print("hase  element send to mail is broken")
 
-chackDatafile("D:\TEMP", "WINservice", "test")
+chackDatafile(os.chdir(workDir), listServiceBeckup)
