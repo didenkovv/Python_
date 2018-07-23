c1c057851123efcc85f3ba8b48e6003f5d627b58
didenkovv <slavatorrent@ukr.net>
2018-07-23 18:01:16 +0300

    add def send Mail

--- Send_to_mail.py
+++ Send_to_mail.py
@@ -1,6 +1,22 @@
 import os, glob
 import datetime
+import smtplib
 
+### credentional for mail
+mailAddressFrom = "systemmanager2019@gmail.com"
+mailAddressFromPassword = "1qa@WS3ed"
+mailAddressTo = "didenko.slava@gmail.com"
+smtpServer = smtplib.SMTP('smtp.gmail.com', 587)
+smtpServer.starttls()
+smtpServer.login(mailAddressFrom, "1qa@WS3ed")
+# successMessageSubject = "This backup work is well"
+successMessageText = "successful beckup"
+brockenMessageText = "brocken beckup"
+
+
+###
+
+###
 ### service name, need set beckup name machines ####
 listServiceBeckup = ["WINservice", "Serv2016_1C"]
 
@@ -25,10 +41,24 @@ def chackDatafile(pathDir, *args ):
         if filename.endswith(extensionFile) and datetime.datetime.fromtimestamp(float(a)).strftime('%Y-%m-%d')==dates and listServiceBeckup.__contains__(filename.rsplit(charFind)[0]):
             tempelementForEquals = filename.rsplit(charFind)[0]
             listBack.append(tempelementForEquals)
-    balanceList = set(listServiceBeckup) - set(listBack)
-    if not balanceList:
+    sendMailBrockenBackup = set(listServiceBeckup) - set(listBack)
+    if not sendMailBrockenBackup:
+        sendMailbrocken(sendMailBrockenBackup)
         print("have not element, -send to mail for secsesfol")
     else:
+        sendMailbrocken(sendMailBrockenBackup)
         print("hase  element send to mail is broken")
 
+def sendMailbrocken(sendMailBrocken):
+    send_mail_brocken = set(sendMailBrocken)
+    mailBodyMassage = "\r\n".join((
+        "From: %s" % mailAddressFrom,
+        "To: %s" % mailAddressTo,
+        "Subject: %s" % "beckup is brocken",
+        "",
+        "beckup is brocken" + send_mail_brocken.__str__()
+    ))
+    smtpServer.sendmail(mailAddressFrom, mailAddressTo, mailBodyMassage)
+    smtpServer.quit()
+### run for test ####
 chackDatafile(os.chdir(workDir), listServiceBeckup)
